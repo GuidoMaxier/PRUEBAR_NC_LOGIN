1f96e39fc4c9629f1e3d055cb65fa2f076724261
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-12-11 01:08:26 -0300

    UPDATE MODELS
    -

--- app/models.py
+++ app/models.py
@@ -22,3 +22,41 @@ class User(db.Model, UserMixin):
         return str(self.id)
     
 
+#############################################################
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(255))
+    is_active = db.Column(db.Boolean, default=True)
+
+class RelationshipUserJob(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id_job = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id_relationship_user_job = db.Column(db.Integer, db.ForeignKey('relationship_user_job.id'), nullable=False)
+    description = db.Column(db.String(255))
+    date_create = db.Column(db.Date)
+    date_start = db.Column(db.Date)
+    date_end = db.Column(db.Date)
+    id_assessment = db.Column(db.Integer, db.ForeignKey('assessment.id'))
+    state = db.Column(db.Enum('Pending', 'InProgress', 'Completed'))
+
+class JobDetail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_relationship_user_job = db.Column(db.Integer, db.ForeignKey('relationship_user_job.id'), nullable=False)
+    description = db.Column(db.String(255))
+    day = db.Column(db.Time)
+    time = db.Column(db.Time)
+
+class Assessment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_service = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    assessment_user_client = db.Column(db.Float)
+    comment_user_client = db.Column(db.String(255))
+    assessment_user_provider = db.Column(db.Float)
+    comment_user_provider = db.Column(db.String(255))
+    state = db.Column(db.Enum('Pending', 'Approved', 'Rejected'))
