53e9dc9e21bdf76e7ef60b60845f10a757bfc6f2
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-12-11 23:32:55 -0300

    Added new features

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,30 +4,6 @@ from flask import request, jsonify
 from . import auth
 from ..models import User, db
 
-# @auth.route("/api/register", methods=['POST'])
-# def register():
-#     data = request.get_json()
-
-#     username = data.get('username')
-#     email = data.get('email')
-#     password = data.get('password')
-
-#     if not username or not email or not password:
-#         return jsonify({'message': 'Incomplete data'}), 400
-
-#     existing_user = User.query.filter_by(username=username).first()
-
-#     if existing_user:
-#         return jsonify({'message': 'Username already exists'}), 400
-
-#     new_user = User(username=username, email=email)
-#     new_user.set_password(password)
-
-#     db.session.add(new_user)
-#     db.session.commit()
-
-#     return jsonify({'message': 'User registered successfully'}), 201
-
 
 @auth.route("/api/register", methods=['POST'])
 def register():
@@ -70,10 +46,10 @@ def login():
     password = data.get('password')
 
     user = User.query.filter_by(username=username).first()
-
+    
     if user and user.check_password(password):
         login_user(user)
-        return jsonify({'message': 'Login successful'}), 200
+        return jsonify({'message': 'Login successful', 'user_id': user.id, 'username': user.username}), 200
     else:
         return jsonify({'message': 'Invalid username or password'}), 401
 
@@ -98,3 +74,12 @@ def get_users():
 @auth.route("/")
 def index():
     return jsonify({'users': "hola"})
+
+
+
+# En /auth/views.py
+@auth.route("/api/logout")
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logout successful'}), 200
