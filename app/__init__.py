6e83cc523a957829f911ce5690d92a5dd06c7fb6
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-12-11 21:39:09 -0300

    Add CORS configuration to allow requests from any origin

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # app/__init__.py
 from flask import Flask
 from .config import Config
-from .extensions import db, migrate, ma, login_manager
+from .extensions import db, migrate, ma, login_manager, configure_cors
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +11,9 @@ def create_app():
     migrate.init_app(app, db)
     ma.init_app(app)
     login_manager.init_app(app)
+
+    # Configuración CORS
+    configure_cors(app)
 
 
     from .models import User
